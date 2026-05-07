aad094df2f417b28d7cab974f03352b95fb0e70f
glebboxik-cmyk <glebboxik@gmail.com>
2026-05-07 22:51:32 +0300

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,24 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    public static int calculate(int[] numbers) {
+        if (numbers == null || numbers.length < 2) {
+            return Integer.MIN_VALUE;
+        }
+
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int number : numbers) {
+            if (number > max) {
+                secondMax = max;
+                max = number;
+            } else if (number > secondMax && number < max) {
+                secondMax = number;
+            }
+        }
+
+        return secondMax;
     }
 }
